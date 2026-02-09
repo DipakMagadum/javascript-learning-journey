b751ea5496889f57e7582262f315ffbfefd37ced
Dipak Magadum <deepakmagdum769@gmail.com>
2026-02-09 04:32:02 +0000

    Conversion of Data types

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,34 @@
+// Number conversion 
+
+let score = "33abc"
+
+//console.log(typeof score); // this check the datatype of variable
+//console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc " => NaN
+// true => 1; false => 0
+
+// Boolean Data Type conversion
+
+let IsLoggedIn = 1
+
+//let BooleanIsLoggedIn = Boolean(IsLoggedIn) // this converts data types in boolean
+// console.log(BooleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+//"Dipak" => true
+
+// String Datatype Conversion 
+
+let SomeNumber = 123
+
+let Stringnumber = String(SomeNumber)
+console.log(Stringnumber);
+console.log( typeof Stringnumber);
+
