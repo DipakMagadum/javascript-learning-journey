8c3af34ca175c0b84ab8fea35cb397119b52c991
dipak <deepakmagdum769@gmail.com>
2026-08-05 20:29:42 +0530

    feat: add JavaScript comments examples

---
+++ 01-Basics/src/02-Comments.js
@@ -0,0 +1,9 @@
+// This is a single line comment
+
+console.log("JavaScript Comments");
+
+/*
+this is a multi line comment
+it can span multiple lines 
+*/
+console.log("Learning JavaScript step by step")
