7362c20bd5401606255545eb2da25fa99ae56a61
dipak <deepakmagdum769@gmail.com>
2026-08-06 22:27:25 +0530

    feat: add console methods examples

---
+++ 01-Basics/src/03-ConsoleMethods.js
@@ -0,0 +1,13 @@
+console.log("Welcome to javaScript");
+
+console.warn("This is a warning message");
+
+console.error("This is a Error message");
+
+const student ={
+    name:"Dipak",
+    rollno:17,
+    city:"Kolhapur"
+};
+
+console.table(student);
