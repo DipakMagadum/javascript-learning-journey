29cd081cfcace810490c1acc1e8a77883821bd0e
dipak <deepakmagdum769@gmail.com>
2026-08-08 08:54:43 +0530

    feat: compare let const and var

---
+++ 01-Basics/src/05-LetConstVar.js
@@ -0,0 +1,11 @@
+let productPrice = 500;
+productPrice = 550;
+
+const shopName="Shopiz";
+
+var customerName = "Amit";
+customerName = "Aman";
+
+console.log(productPrice);
+console.log(shopName);
+console.log(customerName);
