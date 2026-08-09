725e36fcc207b843741a790ac1c90245c0d61902
dipak <deepakmagdum769@gmail.com>
2026-08-09 16:28:28 +0530

    feat: add JavaScript data type examples

---
+++ 01-Basics/src/06-DataTypes.js
@@ -0,0 +1,14 @@
+let studentName="Dipak";
+let studentAge=22;
+let studentMarks = 82.5;
+let isPassed = true;
+
+let phoneNumber;
+let examResult = null;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentMarks);
+console.log(isPassed);
+console.log(phoneNumber);
+console.log(examResult);
