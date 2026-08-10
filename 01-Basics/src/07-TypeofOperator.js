e6998592c4b20db9f05d0b58541b39d2a6b96faf
dipak <deepakmagdum769@gmail.com>
2026-08-10 11:20:40 +0530

    feat: practice typeof operator

---
+++ 01-Basics/src/07-TypeofOperator.js
@@ -0,0 +1,15 @@
+let userName="akshya02";
+let followers=35;
+let following =90;
+
+let isActive=true;
+
+let phoneNumber;
+let email=null;
+
+console.log( typeof userName);
+console.log( typeof followers);
+console.log( typeof following);
+console.log( typeof isActive);
+console.log( typeof phoneNumber);
+console.log( typeof email);
