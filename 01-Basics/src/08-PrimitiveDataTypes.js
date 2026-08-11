292fefd4403663a93a36c0e3ee891161c1be3f9c
dipak <deepakmagdum769@gmail.com>
2026-08-11 12:44:28 +0530

    feat: add primitive data type examples

---
+++ 01-Basics/src/08-PrimitiveDataTypes.js
@@ -0,0 +1,18 @@
+let userName="akshay02";
+let followres="40";
+let isActive =true;
+
+let phoneNumber;
+let email=null;
+
+let population = 9007132432423424242n;
+
+let userId= Symbol("userId");
+
+console.log(userName);
+console.log(followres);
+console.log(isActive);
+console.log(phoneNumber);
+console.log(email);
+console.log(population);
+console.log(userId);
