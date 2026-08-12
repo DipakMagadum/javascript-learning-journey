87b66410dd27cc41770ed89cbbef6f7d7d3f4ef8
dipak <deepakmagdum769@gmail.com>
2026-08-12 19:05:21 +0530

    feat: add non-primitive data type examples

---
+++ 01-Basics/src/09-NonPrimitiveDataTypes.js
@@ -0,0 +1,15 @@
+const employee = {
+    name: "Dipak",
+    age: 22,
+    role: "Software Developer"
+};
+
+const skills =["Java","JavaScript","React"];
+
+function greetEmployee(){
+    console.log("Welcome to our company");
+}
+console.log( "Employee Details :",employee);
+console.log("skills :",skills);
+
+greetEmployee();
