442207b8ec3c620830d1da9cc3dd7aead2f4f904
dipak <deepakmagdum769@gmail.com>
2026-08-13 18:55:24 +0530

    feat: add type conversion examples

---
+++ 01-Basics/src/10-TypeConversion.js
@@ -0,0 +1,11 @@
+let ticketPrice = "250";
+let numberOfTickets = "3";
+
+let price = Number(ticketPrice);
+let tickets = Number(numberOfTickets);
+
+let totalAmount = price * tickets;
+
+console.log(price);
+console.log(tickets);
+console.log(totalAmount);
