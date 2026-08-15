7d99ebaae9a74a862c4897f8fdee6c9f3daab028
dipak <deepakmagdum769@gmail.com>
2026-08-15 21:16:11 +0530

    feat: practice type coercion

---
+++ 01-Basics/src/11-TypeCoercion.js
@@ -0,0 +1,7 @@
+let productPrice = "500";
+let disscount = 70;
+
+console.log(productPrice + disscount);
+console.log(productPrice - disscount);
+console.log(productPrice * disscount);
+console.log(productPrice / disscount);
