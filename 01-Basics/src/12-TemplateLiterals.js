1e725c79673331b3dc0c60bc9026856cc93a7bc0
dipak <deepakmagdum769@gmail.com>
2026-08-16 20:14:56 +0530

    feat: add template literal examples

---
+++ 01-Basics/src/12-TemplateLiterals.js
@@ -0,0 +1,7 @@
+let studentName = "Dipak";
+let studentAge = 23;
+let studentCourse = "MCA";
+
+let message = `${studentName} is ${studentAge} years old and studying ${studentCourse}.`;
+
+console.log(message);
