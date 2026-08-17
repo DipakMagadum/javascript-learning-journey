938511b96d68b4632da666a887bf4ffcd2539c18
dipak <deepakmagdum769@gmail.com>
2026-08-17 14:17:29 +0530

    chore: add strict mode example

---
+++ 01-Basics/src/14-StrictMode.js
@@ -0,0 +1,28 @@
+// program without strict mode 
+
+/*userName = "dipak";
+ let userAge = 22;
+
+ console.log(userName);
+ console.log(userAge);
+ */
+
+ /*
+ OUTPUT=>  dipak
+           22
+
+*/
+
+ // program with strict mode 
+
+ "use strict";
+
+ userStatus = "Active";
+ let userFollowers = 769;
+ console.log(userStatus);
+ console.log(userFollowers);
+
+ /*
+    OUTPUT
+ReferenceError: userStatus is not defined
+ */
