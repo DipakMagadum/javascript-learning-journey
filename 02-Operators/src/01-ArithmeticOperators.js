cb9b0c03d4705eb3da727c58931a5ed6d254378f
dipak <deepakmagdum769@gmail.com>
2026-08-18 08:40:54 +0530

    feat: practice arithmetic operations

---
+++ 02-Operators/src/01-ArithmeticOperators.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 2;
+
+console.log(`The Addition of ${x} and ${y} is : ${x + y}`);
+
+console.log(`The Subtraction of ${x} and ${y} is : ${x - y}`);
+
+console.log(`The Multiplication of ${x} and ${y} is : ${x * y}`);
+
+console.log(`The Division of ${x} and ${y} is : ${x / y}`);
+
+console.log(`The Modulus of ${x} and ${y} is : ${x % y}`);
+
+console.log(`The Exponential of ${x} and ${y} is : ${x ** y}`);
