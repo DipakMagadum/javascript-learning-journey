1bc0d0772934e9f3eba75e0907997e2cf189e094
dipak <deepakmagdum769@gmail.com>
2026-08-19 18:38:00 +0530

    practice assignment operators

---
+++ 02-Operators/src/02-AssignmentOperators.js
@@ -0,0 +1,15 @@
+let accountBalance = 5000;
+
+console.log(accountBalance);
+
+accountBalance += 1000;
+console.log(accountBalance);
+
+accountBalance -= 500;
+console.log(accountBalance);
+
+accountBalance *= 2;
+console.log(accountBalance);
+
+accountBalance /= 2;
+console.log(accountBalance);
