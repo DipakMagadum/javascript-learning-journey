5257c2b0215945fd1b40e459a804f8776ecea388
dipak <deepakmagdum769@gmail.com>
2026-08-20 12:54:42 +0530

    test: verify comparison operators

---
+++ 02-Operators/src/03-ComparisonOperators.js
@@ -0,0 +1,23 @@
+let productPrice = 1200;
+
+let budget = 1500;
+
+console.log(productPrice > budget); // > Greater than
+
+console.log(productPrice < budget); // < Less than
+
+console.log(productPrice >= budget); // >= Greater than or equal to
+
+console.log(productPrice <= budget); // <= Less than or equal to
+
+console.log(productPrice == budget); // == Equal to
+
+console.log(productPrice != budget); // != Not equal to
+
+let number1 = 10;
+
+let number2 = "10";
+
+console.log(number1 == number2); // true 
+
+console.log(number1 === number2); // === Strict equality checks value and data type
