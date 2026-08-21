868470da95139b96f197ddf79ba1dff8a566dc61
dipak <deepakmagdum769@gmail.com>
2026-08-21 16:24:33 +0530

    feat: add logical operator examples

---
+++ 02-Operators/src/04-LogicalOperators.js
@@ -0,0 +1,13 @@
+let isLoggedIn = true;
+let hasAdminAccess = false;
+
+console.log(isLoggedIn && hasAdminAccess); //  AND && both condition must be true
+
+let isWeekend = true;
+let isHoliday = false;
+
+console.log(isWeekend || isHoliday); // OR || atleast one condition must be true
+
+let isAccountBlocked = false;
+
+console.log(!isAccountBlocked); // NOT ! flip the boolean true becomes false and false becomes true
