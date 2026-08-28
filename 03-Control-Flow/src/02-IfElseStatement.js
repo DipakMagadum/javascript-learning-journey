b211961dcac610f6858603bfcfe7561db9155873
dipak <deepakmagdum769@gmail.com>
2026-08-28 09:12:28 +0530

    add if else decision example

---
+++ 03-Control-Flow/src/02-IfElseStatement.js
@@ -0,0 +1,14 @@
+// if (condition) {
+//     // code
+// } else {
+//     // another code
+// }
+
+let loggedIn = true;
+
+if(loggedIn){
+    console.log("Grant Access");
+}
+    else{
+        console.log("Deny Access");
+    }
