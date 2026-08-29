4c1281645217f174158091ac090b464b0d32ab37
dipak <deepakmagdum769@gmail.com>
2026-08-30 00:43:48 +0530

    add nested if example

---
+++ 03-Control-Flow/src/04-NestedIfStatement.js
@@ -0,0 +1,17 @@
+// // syntax
+// if (condition1) {
+
+//     if (condition2) {
+//         // code
+//     }
+
+// }
+
+let isLoggedIn = true;
+let hasProfile = true;
+
+if (isLoggedIn) {
+    if (hasProfile) {
+        console.log("Profile is ready.");
+    }
+}
