4c783923dbf0da210afe15af72633ab94eaa022c
dipak <deepakmagdum769@gmail.com>
2026-09-01 10:29:53 +0530

    test: practice control flow decisions

---
+++ 03-Control-Flow/src/06-ControlFlowPractice.js
@@ -0,0 +1,9 @@
+let orderAmount = 1800;
+
+if (orderAmount >= 2000) {
+    console.log("You get free delivery.");
+} else if (orderAmount >= 1000) {
+    console.log("Delivery charges are reduced.");
+} else {
+    console.log("Delivery charges apply.");
+}
