21115f3b2e9adebbcb74312ff871b7dd97b06dab
dipak <deepakmagdum769@gmail.com>
2026-09-06 12:49:50 +0530

    add continue statement practice

---
+++ 04-Loops/src/05-ContinueStatement.js
@@ -0,0 +1,9 @@
+//The continue statement skips the current iteration of a loop and moves to the next iteration
+for (let i = 1; i <= 5; i++) {
+
+    if (i === 3) {
+        continue;
+    }
+
+    console.log(i);
+}
