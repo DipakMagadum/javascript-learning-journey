4b02b7b087a033db53c3f9cc493ab023e204495a
dipak <deepakmagdum769@gmail.com>
2026-09-10 12:24:04 +0530

    add function parameter examples

---
+++ 05-Functions/src/02-ParametersArguments.js
@@ -0,0 +1,7 @@
+// Parameter => A parameter is a variable defined in a function that receives a value when the function is called
+// Argument => an Argument is the actual value passed to a function when the function is called 
+
+function greet(name){ // here name is a parameter
+    console.log(`Hello ${name}`);
+}
+greet("Dipak"); // here the "Dipak" is an argument
