47734bd70859bc2febeff5d666e45b488fdb65eb
dipak <deepakmagdum769@gmail.com>
2026-09-11 10:08:25 +0530

    add return statement examples

---
+++ 05-Functions/src/03-ReturnStatement.js
@@ -0,0 +1,8 @@
+// return statement => the return statement ends the execution of a function and send the value back to place where the function was called 
+
+function addNumbers(a,b){
+    return a + b; 
+}
+
+let result = addNumbers(5,4);
+console.log(`Result is ${result}`);
