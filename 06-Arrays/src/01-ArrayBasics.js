9358ab2fbb7ee9f21d9c27462be4804e54eab7f6
dipak <deepakmagdum769@gmail.com>
2026-09-15 09:13:14 +0530

    add basic array examples

---
+++ 06-Arrays/src/01-ArrayBasics.js
@@ -0,0 +1,13 @@
+// Array => An array is a data structure used to store multiple values in a single variable 
+
+// Basic Syntax 
+// let arrayName = [value1, value2, value3];
+
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
+
+let products = ["Mobile", "Laptop", "Mouse", "Tab", "Keyboard"];
+console.log(products);
+
+let userDetails = ["Dipak", 22, true]; // we can also store different data types in array
+console.log(userDetails);
