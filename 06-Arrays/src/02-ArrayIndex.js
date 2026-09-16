611e79f9160d697eb140a83f5813ceaf0f17ca28
dipak <deepakmagdum769@gmail.com>
2026-09-16 21:17:51 +0530

    add array index examples

---
+++ 06-Arrays/src/02-ArrayIndex.js
@@ -0,0 +1,10 @@
+// An array index represents the position of an element in an array
+// array index start from zero 
+// how to get a value in array
+// arrayName[index]
+
+let city = ["Kolhapur","Pune","Mumbai","Delhi"]; // array index start with zero
+console.log(city[0]);
+console.log(city[1]);
+console.log(city[2]);
+console.log(city[3]);
