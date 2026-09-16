65b7f57121a349ba4666a1cfeb53f44ef864ad36
dipak <deepakmagdum769@gmail.com>
2026-09-16 21:21:37 +0530

    add array element update example

---
+++ 06-Arrays/src/03-UpdateArrayElements.js
@@ -0,0 +1,18 @@
+// Updating an array element
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits);
+
+fruits[1] = "Orange";
+
+console.log(fruits);
+
+
+// Real-world example
+
+let cartItems = ["Laptop", "Mouse", "Keyboard"];
+
+cartItems[1] = "Webcam";
+
+console.log(cartItems);
