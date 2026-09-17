b589a8b235f659737796d791171b3a8b3d24bd9c
dipak <deepakmagdum769@gmail.com>
2026-09-17 21:09:47 +0530

    add array iteration examples

---
+++ 06-Arrays/src/05-ArrayIteration.js
@@ -0,0 +1,15 @@
+let colors = ["Red", "Blue", "Green", "Black", "White"];
+
+// for...of loop
+
+for (let color of colors){
+    console.log(color);
+    
+}
+
+// forEach 
+
+colors.forEach((color)=> {
+    console.log(`Color ${color}`);
+    
+});
